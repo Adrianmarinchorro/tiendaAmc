88464deeaceadd15e88788854ff65ad1e3bfdeb3
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2022-10-25 10:16:19 +0200

    refactor: separate functionality on update method at AdminUserController

--- app/controllers/AdminUserController.php
+++ app/controllers/AdminUserController.php
@@ -18,7 +18,7 @@ class AdminUserController extends Controller
 
         $users = $this->model->getUser();
 
-        $data =[
+        $data = [
             'titulo' => 'Administracion de Usuarios',
             'menu' => false,
             'admin' => true,
@@ -32,7 +32,7 @@ class AdminUserController extends Controller
     public function create()
     {
 
-        if($_SERVER['REQUEST_METHOD'] != 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 
             $this->showCreateForm();
             return;
@@ -52,52 +52,52 @@ class AdminUserController extends Controller
             'password' => $password1,
         ];
 
-        if(empty($name)) {
+        if (empty($name)) {
             $errors[] = 'El nombre de usuario es requerido';
 
         }
-        if(empty($email)) {
-            $errors[] =  'El email es requerido';
+        if (empty($email)) {
+            $errors[] = 'El email es requerido';
 
         }
-        if(empty($password1)) {
-            $errors[] =  'La contraseña es requerido';
+        if (empty($password1)) {
+            $errors[] = 'La contraseña es requerido';
 
         }
-        if(empty($password2)) {
-            $errors[] =  'La verificacion de contraseña es requerida';
+        if (empty($password2)) {
+            $errors[] = 'La verificacion de contraseña es requerida';
 
         }
 
-        if($password1 != $password2) {
+        if ($password1 != $password2) {
             $errors[] = 'Las claves no coinciden';
 
         }
 
-        if(! $errors ){
+        if (!$errors) {
 
-           if( $this->model->createAdminUser($dataForm)){
+            if ($this->model->createAdminUser($dataForm)) {
 
-               header('location:' . ROOT . 'adminUser');
-               return;
+                header('location:' . ROOT . 'adminUser');
+                return;
 
             }
 
-           $data = [
-               'titulo' => 'Error en la creacion de un usuario administrador',
-               'menu' => false,
-               'errors' => [],
-               'subtitle' => 'Error al crear un nuevo usuario administrador',
-               'text' => 'Se ha producido un error durante el proceso de creacion de un usuario administrador',
-               'color' => 'alert-danger',
-               'url' => 'adminUser',
-               'colorButton' => 'btn-danger',
-               'textButton' => 'Volver',
-           ];
+            $data = [
+                'titulo' => 'Error en la creacion de un usuario administrador',
+                'menu' => false,
+                'errors' => [],
+                'subtitle' => 'Error al crear un nuevo usuario administrador',
+                'text' => 'Se ha producido un error durante el proceso de creacion de un usuario administrador',
+                'color' => 'alert-danger',
+                'url' => 'adminUser',
+                'colorButton' => 'btn-danger',
+                'textButton' => 'Volver',
+            ];
 
-           $this->view('mensaje', $data);
+            $this->view('mensaje', $data);
 
-           return;
+            return;
 
 
         }
@@ -135,45 +135,49 @@ class AdminUserController extends Controller
 
         $errors = [];
 
+        if($_SERVER['REQUEST_METHOD'] != 'POST'){
+            $this->updateView($id);
+            return;
+        }
+
         //TODO: Refactorizar el POST (carlos) entero e incluso el metodo en si
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            $name = $_POST['name'] ?? '';
-            $email = $_POST['email'] ?? '';
-            $password1 = $_POST['password'] ?? '';
-            $password2 = $_POST['password2'] ?? '';
-            $status = $_POST['status'] ?? '';
+        $name = $_POST['name'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $password1 = $_POST['password'] ?? '';
+        $password2 = $_POST['password2'] ?? '';
+        $status = $_POST['status'] ?? '';
 
-            if ($name == '') {
-                $errors[] = 'El nombre del usuario es requerido';
-            }
-            if ($email == '') {
-                $errors[] = 'El email es requerido';
-            }
-            if ($status == '') {
-                $errors[] = 'Selecciona un estado para el usuario';
-            }
-            if ( !empty($password1) || !empty($password2)) {
-                if ($password1 != $password2) {
-                    $errors[] = 'Las contraseñas no coinciden';
-                }
-            }
-
-            if ( ! $errors ) {
-                $data = [
-                    'id' => $id,
-                    'name' => $name,
-                    'email' => $email,
-                    'password' => $password1,
-                    'status' => $status,
-                ];
-                $errors = $this->model->setUser($data);
-                if ( ! $errors ) {
-                    header("location:" . ROOT . 'adminUser');
-                }
+        if ($name == '') {
+            $errors[] = 'El nombre del usuario es requerido';
+        }
+        if ($email == '') {
+            $errors[] = 'El email es requerido';
+        }
+        if ($status == '') {
+            $errors[] = 'Selecciona un estado para el usuario';
+        }
+        if (!empty($password1) || !empty($password2)) {
+            if ($password1 != $password2) {
+                $errors[] = 'Las contraseñas no coinciden';
             }
         }
 
+        if (!$errors) {
+            $data = [
+                'id' => $id,
+                'name' => $name,
+                'email' => $email,
+                'password' => $password1,
+                'status' => $status,
+            ];
+            $errors = $this->model->setUser($data);
+            if (!$errors) {
+                header("location:" . ROOT . 'adminUser');
+            }
+        }
+
+        /*
         $user = $this->model->getUserById($id);
         $status = $this->model->getConfig('adminStatus');
 
@@ -187,8 +191,25 @@ class AdminUserController extends Controller
         ];
 
         $this->view('admin/users/update', $data);
+*/
+
+    }
+
+    public function updateView($id)
+    {
+        $user = $this->model->getUserById($id);
+        $status = $this->model->getConfig('adminStatus');
 
 
+        $data = [
+            'titulo' => 'Administracion de usuarios - Editar',
+            'menu' => false,
+            'admin' => true,
+            'status' => $status,
+            'data' => $user,
+        ];
+
+        $this->view('admin/users/update', $data);
     }
 
     //TODO: Refactor carlos delete
@@ -201,7 +222,7 @@ class AdminUserController extends Controller
 
             $errors = $this->model->delete($id);
 
-            if ( ! $errors ) {
+            if (!$errors) {
                 header('location:' . ROOT . 'adminUser');
             }
 
